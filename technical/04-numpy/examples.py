b5a973daba81c2a3ee587ec5285def3d81e7095c
MSF998 <mohdsahnoon@gmail.com>
2026-07-16 19:13:50 +0530

    Enhance Numpy examples with inline comments for clarity on array sums and axis definitions

--- technical/04-numpy/examples.py
+++ technical/04-numpy/examples.py
@@ -1,10 +1,11 @@
 import numpy as np
 
 x = np.array([
-    [1, 2, 3],
-    [50,60,70],
-    [0, 1, 0]
+    [1, 2, 3], # 6
+    [50,60,70], # 180
+    [0, 1, 0] # 1
+    # 51, 63, 73
 ])
 
-# number of axis = number of dimension
-# Axis 0 = Rows
+print(x.sum(axis=1)) # moving horizontally across, Column #[  6 180   1]
+print(x.sum(axis=0)) # moving vertically down, Row #[51 63 73]
